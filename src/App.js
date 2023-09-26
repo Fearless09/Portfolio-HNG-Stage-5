8340d4833a937ec7f085aa20a8d1be6f957ce2b2
AJAYI Toheeb Opeyemi <toheebopeyemi9@gmail.com>
2023-09-26 14:28:18 +0100

    Done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.scss";
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
 import Home from "./containers/home";
@@ -12,36 +12,30 @@ import Navbar from "./components/navBar";
 import particles from "./utils.js/particles";
 
 function App() {
-  const location = useLocation();
-  console.log(location);
 
   const handleInit = async (main) => {
     await loadFull(main);
   };
 
-  const renderParticleJsInHomePage = location.pathname === "/";
-
   return (
     <div className="App">
       {/* particles js */}
 
-      {renderParticleJsInHomePage && (
-        <Particles id="particles" options={particles} init={handleInit} />
-      )}
+      <Particles id="particles" options={particles} init={handleInit} />
 
       {/* navbar */}
       <Navbar />
 
       {/* main page content */}
       <div className="App__main-page-content">
-      <Routes>
-        <Route index path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/resume" element={<Resume />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+        <Routes>
+          <Route index path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
       </div>
 
     </div>
